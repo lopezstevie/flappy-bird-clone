ed37c1a9f0afe39614c166ba1a158d5116c0fd3f
Steffanie Lopez <156367029+lopezstevie@users.noreply.github.com>
2025-03-08 00:08:01 -0800

    render multiple pipes

--- src/index.js
+++ src/index.js
@@ -10,7 +10,6 @@ const config = {
     // Arcade physics plugin, manages physics simulation
     default: 'arcade',
     arcade: {
-      gravity: { y: 400 },
       debug: true
     }
   },
@@ -21,20 +20,42 @@ const config = {
   }
 }
 
+const VELOCITY = 200;
+const PIPES_TO_RENDER = 4;
+
+let bird = null;
+
+let upperPipe = null;
+let lowerPipe = null;
+let pipeHorizontalDistance = 0;
+
+const pipeVerticalDistantRange = [150, 250];
+
+const flapVelocity = 250;
+const initialBirdPosition = {x: config.width / 10, y: config.height / 2};
+
 function preload() {
   this.load.image('sky', 'assets/sky.png');
   this.load.image('bird', 'assets/bird.png');
+  this.load.image('pipe', 'assets/pipe.png');
 }
-
-const VELOCITY = 200;
-
-let bird = null;
-const flapVelocity = 250;
-const initialBirdPosition = {x: config.width / 10, y: config.height / 2};
 
 function create() {
   this.add.image(0, 0, 'sky').setOrigin(0);
   bird = this.physics.add.sprite(initialBirdPosition.x, initialBirdPosition.y, 'bird').setOrigin(0);
+  bird.body.gravity.y = 400; 
+
+  for (let i = 0; i < PIPES_TO_RENDER; i++) {
+    pipeHorizontalDistance += 400;
+    let pipeVerticalDistance = Phaser.Math.Between(...pipeVerticalDistantRange);
+    let pipeVerticalPosition = Phaser.Math.Between(20, config.height - 20 - pipeVerticalDistance);
+
+    upperPipe = this.physics.add.sprite(pipeHorizontalDistance, pipeVerticalPosition, 'pipe').setOrigin(0, 1);
+    lowerPipe = this.physics.add.sprite(upperPipe.x, upperPipe.y + pipeVerticalDistance, 'pipe').setOrigin(0, 0);
+
+    upperPipe.body.velocity.x = -200;
+    lowerPipe.body.velocity.x = -200;
+  }
 
   this.input.on('pointerdown', flap);
   this.input.keyboard.on('keydown-SPACE', flap);
