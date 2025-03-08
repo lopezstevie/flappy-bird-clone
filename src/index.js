35db5c38d65db96a54156d12564e817f9376f3e1
Steffanie Lopez <156367029+lopezstevie@users.noreply.github.com>
2025-03-07 23:36:40 -0800

    Add restart game function

--- src/index.js
+++ src/index.js
@@ -29,19 +29,27 @@ function preload() {
 const VELOCITY = 200;
 
 let bird = null;
-let flapVelocity = 250;
-let totalDelta = null;
+const flapVelocity = 250;
+const initialBirdPosition = {x: config.width / 10, y: config.height / 2};
 
 function create() {
   this.add.image(0, 0, 'sky').setOrigin(0);
-  bird = this.physics.add.sprite(config.width / 10, config.height / 2, 'bird').setOrigin(0);
+  bird = this.physics.add.sprite(initialBirdPosition.x, initialBirdPosition.y, 'bird').setOrigin(0);
 
   this.input.on('pointerdown', flap);
   this.input.keyboard.on('keydown-SPACE', flap);
 }
 
 function update(time, delta) {
+  if (bird.y < -bird.height || bird.y > config.height) {
+    restartBirdPosition();
+  }
+}
 
+function restartBirdPosition() {
+  bird.x = initialBirdPosition.x;
+  bird.y = initialBirdPosition.y;
+  bird.body.velocity.y = 0;
 }
 
 function flap() {
