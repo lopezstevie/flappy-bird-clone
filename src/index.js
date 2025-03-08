bac45e4ac86fb5485e46123a894f188f8aa09886
Steffanie Lopez <156367029+lopezstevie@users.noreply.github.com>
2025-03-07 22:42:35 -0800

    Setup basic background and bird spriate

--- src/index.js
+++ src/index.js
@@ -1,28 +1,37 @@
-
 import Phaser from "phaser";
 
 const config = {
+  // WebGL (Web graphics library) JS API for rendering 2D and 3D graphics
   type: Phaser.AUTO,
   width: 800,
   height: 600,
   physics: {
-    default: 'arcade',
-    arcade: {
-      gravity: { y: 200 }
-    }
+    // Arcade physics plugin, manages physics simulation
+    default: 'arcade'
   },
   scene: {
-    preload: preload,
-    create: create
+    preload,
+    create
   }
-};
+}
+
+// Loading assets, such as images, music, animations ...
+function preload() {
+  // 'this' context - scene
+  // contains functions and properties we can use
+  this.load.image('sky', 'assets/sky.png');
+  this.load.image('bird', 'assets/bird.png');
+}
+
+let bird = null;
+
+function create() {
+  // x - 400
+  // y - 300
+  // key of the image
+  this.add.image(0, 0, 'sky').setOrigin(0);
+  // middle of the height, 1/10 width
+  bird = this.add.sprite(config.width / 10, config.height / 2, 'bird').setOrigin(0);
+}
 
 new Phaser.Game(config);
-
-function preload () {
-  this.load.image('sky', 'assets/sky.png');
-}
-
-function create () {
-  this.add.image(400, 300, 'sky');
-}
