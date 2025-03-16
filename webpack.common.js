32dba48ebf590f62a205633c02b330b843e84591
Steffanie Lopez <156367029+lopezstevie@users.noreply.github.com>
2025-03-16 16:51:38 -0700

    Ready for gh deployment

--- webpack.common.js
+++ webpack.common.js
@@ -6,9 +6,9 @@ const webpack = require('webpack');
 module.exports = {
   mode: 'development',
   entry: {
-    app: './src/index.js'
+    app: './src/index.js',
   },
-  devtool: "eval-source-map",
+  devtool: 'eval-source-map',
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, 'build'),
@@ -19,10 +19,10 @@ module.exports = {
         commons: {
           test: /[\\/]node_modules[\\/]/,
           name: 'vendor',
-          chunks: 'all'
-        }
-      }
-    }
+          chunks: 'all',
+        },
+      },
+    },
   },
   module: {
     rules: [
@@ -31,33 +31,40 @@ module.exports = {
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
-        }
-      }
-    ]
+        },
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
   },
   devServer: {
     contentBase: path.resolve(__dirname, 'build'),
     compress: true,
     port: 8080,
   },
+  resolve: {
+    extensions: ['.js'],
+  },
   plugins: [
     new webpack.DefinePlugin({
-      'CANVAS_RENDERER': JSON.stringify(true),
-      'WEBGL_RENDERER': JSON.stringify(true)
+      CANVAS_RENDERER: JSON.stringify(true),
+      WEBGL_RENDERER: JSON.stringify(true),
     }),
     new HtmlWebpackPlugin({
-      template: './index.html'
+      template: './index.html',
     }),
     new CopyPlugin({
       patterns: [
         {
-          from: path.resolve(__dirname, 'assets/**/*'),
-          to: path.resolve(__dirname, 'build')
-        }
+          from: path.resolve(__dirname, 'assets'),
+          to: path.resolve(__dirname, 'docs/assets')
+        },
       ],
     }),
     new webpack.ProvidePlugin({
-      process: 'process/browser' 
-    })
+      process: 'process/browser',
+    }),
   ],
 };
--- webpack.prod.js
+++ webpack.prod.js
@@ -5,23 +5,26 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
+  output: {
+    filename: '[name].js',
+    path: __dirname + '/docs',
+    clean: true,
+  },
   devtool: false,
   performance: {
     maxEntrypointSize: 90000,
-    maxAssetSize: 900000
+    maxAssetSize: 900000,
   },
   optimization: {
     minimizer: [
       new TerserPlugin({
         terserOptions: {
           output: {
-            comments: false
-          }
-        }
-      })
-    ]
+            comments: false,
+          },
+        },
+      }),
+    ],
   },
-  plugins: [
-    new CleanWebpackPlugin(),
-  ]
-})
+  plugins: [new CleanWebpackPlugin()],
+});
